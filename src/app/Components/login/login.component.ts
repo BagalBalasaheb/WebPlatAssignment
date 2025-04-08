fd98e4cbd204ef3424ed109dc4d486b25370647d
BagalBalasaheb <balasaheb.bagal19@vit.edu>
2025-04-08 23:46:01 +0530

    Formating

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -27,8 +27,8 @@ export class LoginComponent {
     if (this.form.valid) {
       this.authService.login(this.form.value).subscribe({
         next: (response) => {
-          localStorage.setItem('token', response.accessToken); 
-          this.router.navigate(['/dashboard/profile']); 
+          localStorage.setItem('token', response.accessToken);
+          this.router.navigate(['/dashboard/profile']);
         },
         error: () => {
           alert('Invalid credentials');
